769712fc88145a0dbdbb2b4d3b3b742f5a303f1c
Vivek Pillai <vpillai@ctisinc.com>
2018-11-24 10:54:17 -0500

    Dynamic ...

---
+++ src/components/compound/index.tsx
@@ -0,0 +1,8 @@
+import TextField from './TextField';
+import ExampleFunctionComponent from './ExampleFunctionComponent';
+const Controls = {
+  TextField,
+  ExampleFunctionComponent
+};
+
+export default Controls;
--- src/components/scheme/Waterfall.tsx
+++ src/components/scheme/Waterfall.tsx
@@ -1,18 +1,32 @@
 // tslint:disable
 import React from 'react';
-import { withApollo } from 'react-apollo';
+import { lazy } from 'react';
+import DynamicComponent from '../framework/DynamicComponent';
 
 function Waterfall(props: any) {
+  const DynamicModule = (language: string) => {
+    switch (language) {
+      case 'Dynamic.ExampleFunctionComponent': {
+        return lazy(() => import('../compound/ExampleFunctionComponent'));
+      }
+      // ...etc
+      // We know the list of languages, because we created a source file for each one!
+    }
+  };
+
+  function renderDynamicComponents(children: any) {
+    const keys = Object.keys(children);
+    return keys.map((key: string) => {
+      return <DynamicComponent component={children[key].component} />;
+    });
+  }
+
   console.log('Waterfall');
   //console.log(props);
   const componentList = props.children;
   console.log(componentList);
   console.log(componentList['0000']);
-  let componentName;
-  for (let value in componentList) {
-    console.log(value);
-  }
-  return <></>;
+  return <>{renderDynamicComponents(componentList)}</>;
 }
 
 export default Waterfall;
