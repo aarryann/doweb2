ab11af25d37e23b3af4e084cb042681d72c6bed8
Vivek Pillai <vpillai@ctisinc.com>
2018-12-11 17:56:51 -0500

    generator...

---
+++ lexer.js
@@ -0,0 +1,200 @@
+// tslint:disable:no-console
+const fs = require('fs');
+const readline = require('readline');
+const yaml = require('js-yaml');
+
+const parseSyntax = (category, appConfig) => {
+  let parsedSyntax = {};
+  const viewKeys = Object.keys(appConfig.Views);
+  for (let view of viewKeys) {
+    // console.log(JSON.stringify(appConfig.Views[view], null, 4));
+    let viewConfig = appConfig.Views[view];
+    if (viewConfig.category.toLowerCase() !== category.toLowerCase()) {
+      continue;
+    }
+    parsedSyntax[view] = {};
+    parsedSyntax[view]['#COMPONENT_NAME#'] = view + category;
+    parsedSyntax[view]['#DECLARE_PROPS#'] = '';
+    parsedSyntax[view]['#DECLARE_DATASOURCE#'] = '';
+    const childrenKeys = Object.keys(viewConfig.children);
+    for (let index of childrenKeys) {
+      let componentConfig = viewConfig.children[index];
+      parsedSyntax[view]['#DECLARE_PROPS#'] += getSyntax(
+        '#DECLARE_PROPS#',
+        index,
+        componentConfig
+      );
+      parsedSyntax[view]['#DECLARE_DATASOURCE#'] += getSyntax(
+        '#DECLARE_DATASOURCE#',
+        index,
+        componentConfig
+      );
+    }
+  }
+
+  console.log(JSON.stringify(parsedSyntax, null, 4));
+  return parsedSyntax;
+};
+
+const getSyntax = (syntax, index, componentConfig) => {
+  switch (syntax) {
+    case '#DECLARE_PROPS#': {
+      return `const props${index} = props.children['${index}'].props;\r\n`;
+    }
+    case '#DECLARE_DATASOURCE#': {
+      return `const [results${index}, setResults${index}] = Datasources.${
+        componentConfig.dataSource
+      }(props.client);\r\n`;
+    }
+  }
+};
+
+const transformSyntax = (line, syntax, parsedSyntaxView) => {
+  if (line.indexOf(syntax) >= 0) {
+    return line.replace(syntax, parsedSyntaxView[syntax]);
+  } else {
+    return line;
+  }
+};
+
+const convertSyntax = (line, parsedSyntaxView) => {
+  let transformedLine = transformSyntax(
+    line,
+    '#COMPONENT_NAME#',
+    parsedSyntaxView
+  );
+  transformedLine = transformSyntax(
+    transformedLine,
+    '#DECLARE_PROPS#',
+    parsedSyntaxView
+  );
+  transformedLine = transformSyntax(
+    transformedLine,
+    '#DECLARE_DATASOURCE#',
+    parsedSyntaxView
+  );
+  transformedLine = transformSyntax(
+    transformedLine,
+    '#DECLARE_ACTIONS#',
+    parsedSyntaxView
+  );
+  transformedLine = transformSyntax(
+    transformedLine,
+    '#LIST_COMPONENTS#',
+    parsedSyntaxView
+  );
+  return transformedLine;
+};
+
+const generateComponent = (cat, componentId, parsedSyntaxView) => {
+  const readStream = fs.createReadStream(
+    `./src/templates/${cat.toLowerCase()}.tpl`
+  );
+  const writeStream = fs.createWriteStream(
+    `./src/out/${componentId}.${cat.toLowerCase()}.tsx`,
+    {
+      encoding: 'utf8'
+    }
+  );
+
+  const rl = readline.createInterface({
+    input: readStream,
+    //    output: writeStream,
+    terminal: false,
+    historySize: 0
+  });
+
+  rl.on('line', line => {
+    let transformedLine = line;
+    if (line.indexOf('#') >= 0) {
+      transformedLine = convertSyntax(line, parsedSyntaxView);
+    }
+    // console.log(line);
+    // Do your stuff ...
+    // const transformedLine = line.toUpperCase();
+    // console.log(transformedLine);
+
+    // Then write to outstream
+    writeStream.write(transformedLine + '\r\n');
+  });
+};
+
+const tokenize = (cat, line, lexMap) => {
+  let wipLine = line;
+  const wip = lexMap[cat]['WIP'];
+  if (!lexMap[cat]['counter']) {
+    lexMap[cat]['counter'] = 0;
+  }
+  const match = true; //Placeholder
+  if (wip.length > 0) {
+    wipLine += wip + line;
+  }
+  if (wipLine.indexOf('<$') >= 0) {
+    const openCount = wipLine.match(/<\$REPEAT/g);
+    const closeCount = wipLine.match(/<\$ENDREPEAT/g);
+    if (openCount === closeCount) {
+      const idx = lexMap[cat]['counter']++;
+      lexMap[cat][`$TAG_${idx}`] = wipLine;
+    }
+  }
+  // Placeholder
+  if (match) {
+    lexMap[cat]['KEY'] = wipLine;
+  } else {
+    lexMap[cat]['WIP'] = wipLine;
+  }
+
+  return [line, lexMap, true];
+};
+
+const extractTokens = 
+
+const lexer = (cat, viewConfig, lexMap) => {
+  const readStream = fs.createReadStream(
+    `./src/templates/${cat.toLowerCase()}.tpl`
+  );
+  const writeStream = fs.createWriteStream(
+    `./src/out/staged.${cat.toLowerCase()}.tmp`,
+    {
+      encoding: 'utf8'
+    }
+  );
+
+  lexMap[cat] = {};
+
+  const rl = readline.createInterface({
+    input: readStream,
+    //    output: writeStream,
+    terminal: false,
+    historySize: 0
+  });
+
+  rl.on('line', line => {
+    // Do your stuff ...
+    let [transformedLine, lexMap, transform] = tokenize(cat, line, lexMap);
+
+    // Then write to outstream
+    if (transform) {
+      writeStream.write(transformedLine + '\r\n');
+    }
+  });
+
+  return lexMap;
+};
+
+const categories = ['Mgen'];
+const appConfig = yaml.safeLoad(
+  fs.readFileSync('./src/config/viewconfig.yaml', 'utf8')
+);
+
+let lexMap = {};
+for (let category of categories) {
+  lexMap = lexer(category, appConfig.Views, lexMap);
+
+  const viewKeys = Object.keys(parsedSyntax);
+  for (let view of viewKeys) {
+    // console.log(JSON.stringify(appConfig.Views[view], null, 4));
+    let componentSyntax = parsedSyntax[view];
+    generateComponent(category, view, componentSyntax);
+  }
+}
--- src/templates/mgen.tpl
+++ src/templates/mgen.tpl
@@ -4,16 +4,56 @@ import { Components } from '../controlled';
 import { IManagerProps } from '../controlled/interfaces';
 import { Datasources } from '../data';
 
-export default function #COMPONENT_NAME#(props: IManagerProps) {
-  #DECLARE_PROPS#
+export default function #=ROOT.entity##=ROOT.category>(props: IManagerProps) {
+  <$REPEAT=ROOT.children>const props#=ROOT.children.ALL# = props.children['#=ROOT.children.ALL#'].props;<$ENDREPEAT>
   // Combine data and fetch into single state
-  #DECLARE_DATASOURCE#
+  const [results0000, setResults0000] = Datasources.Boards.useSubOwnBoard(
+    props.client
+  );
+  const [results0100, setResults0100] = Datasources.Boards.useSubOtherBoard(
+    props.client
+  );
 
-  #DECLARE_ACTIONS#
+  const dispatch0000 = (action: string, payload: any) => {
+    switch (action) {
+      case 'createBoard': {
+        Datasources.Boards.createBoard(payload, results0000, setResults0000);
+        break;
+      }
+      case 'viewBoard': {
+        Datasources.Boards.viewBoard(payload, results0000, setResults0000);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  const dispatch0100 = (action: string, payload: any) => {
+    switch (action) {
+      case 'viewBoard': {
+        Datasources.Boards.viewBoard(payload, results0100, setResults0100);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
 
   return (
     <>
-      #LIST_COMPONENTS#
+      <Components.ListComponent
+        dataSource={results0000}
+        dispatch={dispatch0000}
+        {...props0000}
+      />
+      <Components.ListComponent
+        dataSource={results0100}
+        dispatch={dispatch0100}
+        {...props0100}
+      />
     </>
   );
 }
