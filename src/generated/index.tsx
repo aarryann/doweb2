1c2c9f96fa6c1c36491b1d006f0313a4861ecae6
Vivek Pillai <vpillai@ctisinc.com>
2019-01-21 14:28:06 -0500

    gen...

--- src/generated/index.tsx
+++ src/generated/index.tsx
@@ -1,5 +1,7 @@
-import ListCasesMgen from "./ListCases.mgen.gen";
+import ListBoardsMgen from './ListBoards.mgen.gen';
+import ListCasesMgen from './ListCases.mgen.gen';
 
 export const Generated = {
+  ListBoardsMgen,
   ListCasesMgen
 };
