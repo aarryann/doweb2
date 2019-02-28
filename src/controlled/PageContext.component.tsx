3822b9fb75c2a614f61f5e5720408a88b1ee1435
Vivek Pillai <vpillai@ctisinc.com>
2019-02-28 18:10:13 -0500

    context...

--- src/controlled/PageContext.component.tsx
+++ src/controlled/PageContext.component.tsx
@@ -1,12 +1,29 @@
 // tslint:disable
-import React from 'react';
+import React, { useState } from 'react';
 declare const process: IProcess;
 import './PageContext.component.scss';
 
 export default function PageContextComponent(props: any) {
+  const [clipped, setClipped] = useState(false);
+  const handleClipHeader = () => {
+    setClipped(clip => !clip);
+  };
   return (
-    <div className="context-header">
-      <h4>Context Header</h4>
+    <div className={`context-header ${clipped ? 'small-header' : ''}`}>
+      <div className="card">
+        <div className="card-body">
+          <a
+            className="small-header-action"
+            onClick={handleClipHeader}
+            href="javascript:;"
+          >
+            <div className="clip-header">
+              <i className="fa fa-arrow-up" />
+            </div>
+          </a>
+          <h4>Context Header</h4>
+        </div>
+      </div>
     </div>
   );
 }
